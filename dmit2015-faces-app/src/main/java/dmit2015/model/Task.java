6a6c275c4c932a59d4dc1d41716ee3e4e1df874f
Sam Wu <swu@nait.ca>
2025-09-22 18:07:45 -0600

    Added TaskService and MemoryTaskServer. Updated Task.

--- dmit2015-faces-app/src/main/java/dmit2015/model/Task.java
+++ dmit2015-faces-app/src/main/java/dmit2015/model/Task.java
@@ -1,15 +1,26 @@
 package dmit2015.model;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter @Setter
-@ToString
+@Data
+@NoArgsConstructor
 public class Task {
 
+    private String id;  // unique identifier
     private String description;
     private String priority;    // Low, Medium, High
     private boolean done;
 
+    // Copy constructor
+    public Task(Task other) {
+        this.id = other.getId();
+        this.description = other.getDescription();
+        this.priority = other.getPriority();
+        this.done = other.isDone();
+    }
+
+    public static Task copyOf(Task other) {
+        return new Task(other);
+    }
 }
