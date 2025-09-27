d8f6698474fade994f44c8438d92cc485aed3349
Sam Wu <swu@nait.ca>
2025-09-26 20:08:27 -0600

    Added WeatherForecast and memory impl of WeatherForecast CRUD view module.

---
+++ dmit2015-faces-app/src/main/java/dmit2015/model/WeatherForecast.java
@@ -0,0 +1,44 @@
+package dmit2015.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.datafaker.Faker;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+public class WeatherForecast {
+
+    private String id;
+    private String city;
+    private LocalDate date;
+    private int temperatureCelsius;
+
+    public int getTemperatureFahrenheit() {
+        return 32 + (int) (temperatureCelsius / 0.5556);
+    }
+
+    // copy constructor
+    public WeatherForecast(WeatherForecast other) {
+        this.id = other.id;
+        this.city = other.city;
+        this.date = other.date;
+        this.temperatureCelsius = other.temperatureCelsius;
+    }
+
+    public static WeatherForecast copyOf(WeatherForecast other) {
+        return new WeatherForecast(other);
+    }
+
+    public static WeatherForecast of(Faker faker) {
+        var newWeatherForecast = new WeatherForecast();
+        newWeatherForecast.setId(UUID.randomUUID().toString());
+        newWeatherForecast.setCity(faker.address().city());
+        newWeatherForecast.setDate(LocalDate.now().plusDays(1));
+        newWeatherForecast.setTemperatureCelsius(faker.number().numberBetween(-35, 35));
+        return newWeatherForecast;
+    }
+
+}
