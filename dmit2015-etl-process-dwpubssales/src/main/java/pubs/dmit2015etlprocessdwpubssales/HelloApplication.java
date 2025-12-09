8ac85b672990f0c150d332ba932fa702357287e0
Sam Wu <swu@nait.ca>
2025-12-08 20:46:13 -0700

    Added dmit2015-etl-process-dwpubssales demo project for ETL processing

---
+++ dmit2015-etl-process-dwpubssales/src/main/java/pubs/dmit2015etlprocessdwpubssales/HelloApplication.java
@@ -0,0 +1,9 @@
+package pubs.dmit2015etlprocessdwpubssales;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/restapi")
+public class HelloApplication extends Application {
+
+}
